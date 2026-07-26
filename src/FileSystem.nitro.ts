b3438861e70efd6a17faf41072b9f0587a0e7642
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-07-25 21:34:02 -0400

    chore: revert introduction of `FileSystem` nitro module
    - I don't use the `load()` method and it might fix the following error we get on Android 8:
    ```
    NitroModulesProxy.createHybridObject(...): java.lang.NoSuchMethodError: no non-static
    method "Lcom/margelo/nitro/audioanalyzer/FileSystem;.<init>()V" at
    com.facebook.jni.NativeRunnable.run(NativeMethod)
    ```

--- src/FileSystem.nitro.ts
+++
@@ -1,9 +0,0 @@
-import type { HybridObject } from 'react-native-nitro-modules';
-
-export interface FileSystem
-  extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
-  /**
-   * Downloads and caches the file.
-   */
-  load(path: string): Promise<string>;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import { NitroModules } from 'react-native-nitro-modules';
 import type { Processor } from './Processor.nitro';
-import type { FileSystem } from './FileSystem.nitro';
 
 const AudioAnalyzerHybridObject =
   NitroModules.createHybridObject<Processor>('Processor');
-
-const FileSystemHybridObject =
-  NitroModules.createHybridObject<FileSystem>('FileSystem');
 
 function computeAmplitude(filePath: string, outputSampleCount: number) {
   return AudioAnalyzerHybridObject.computeAmplitude(
@@ -15,8 +11,4 @@ function computeAmplitude(filePath: string, outputSampleCount: number) {
   );
 }
 
-function load(path: string) {
-  return FileSystemHybridObject.load(path);
-}
-
-export { load, computeAmplitude };
+export { computeAmplitude };
